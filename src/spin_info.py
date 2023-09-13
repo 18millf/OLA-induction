63a27f29714887bf23bb5ade6418e95ecc086325
Millzy <millzyyt08@gmail.com>
2023-09-13 13:22:20 +0100

    change to dictionary

--- src/spin_info.py
+++ src/spin_info.py
@@ -1,6 +1,6 @@
 from bet_info import BetInfo
 
 class SpinInfo:
-    def __init__(self, result: int, bets: list) -> None:
+    def __init__(self, result: int, bets: dict) -> None:
         self.result = result
         self.bets = bets
